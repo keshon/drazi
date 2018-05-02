70ef6931d3c999333a061f50f0e48896e564a03f
keshon <keshon@zoho.com>
2018-05-02 22:56:36 +0300

    Update get_sar.php

--- get_sar.php
+++ get_sar.php
@@ -1,10 +1,10 @@
 <?php
 //-------------------
-// SAR info parser
-// The output is an array filled with usage data (and some empty keys):
+// SAR info parser for CPU, RAM and Network
+// The output is an array filled with usage data:
 //-------------------
 
-function get_sar_info($atr = 'cpu') {
+function get_sar_info($atr = 'network') {
 
 	switch ($atr) {
 		case 'cpu':
@@ -18,13 +18,13 @@ function get_sar_info($atr = 'cpu') {
 			break;
 	}
 
-	$data = shell_exec('LC_TIME=en_UK.utf8 sar ' . $atr); //execute 'sar' utility with UK locale parameter so the time format is 24H
-	$data = explode("\n", $data); // divide output by lines and refill the array
-	$data = array_slice($data, 3, -2); // offset the array so we skip header and footer	
+	$data = shell_exec('LC_TIME=en_UK.utf8 sar ' . $atr);	//execute 'sar' utility with UK locale parameter so the time format is 24H
+	$data = explode("\n", $data);							// divide output by lines and refill the array
+	$data = array_slice($data, 1); 							// slice the array so we skip header which occurs every 24 hours	
 
-	$out = array(); // declare the output array
+	$out = array(); 										// declare the output array
 
-	// Loop throw array and remove all spaces possible. Only data is allowed
+	// Loop throw data array and remove all spaces between columns
 	foreach ($data as $key => $item)
 	{
 		$data[$key] = preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $data[$key]);
@@ -32,42 +32,43 @@ function get_sar_info($atr = 'cpu') {
 		$data[$key] = explode(" ", $data[$key]);
 	}
 	
-	// Loop throw array and filter non-data values. First level
+	// Loop throw data array to filter non-data values and send data values to output array
 	foreach ($data as $key_row => $row_array)
 	{
 		// Loop throw each key in a row. Second level
 		foreach ($row_array as $key => $var)
 		{
-			// Filter non-data values like labels, info strings and force them to 0
-			if (!preg_match('/All|Average|LINUX|CPU|%/i',$var)) {
-				$row_array[$key] = $var;
+			// Filter non-data values like labels, info strings and set them to 0
+			// This is an indication if the server was restarted
+			if (!preg_match('/All|LINUX|CPU|kb|%/i',$var)) {
+				$row_array[$key] = str_replace(' ','',$var);
 			} else {
 				$row_array[$key] = '0';
 			}
-			// Some nasty exceptions
-			if (!$row_array[$key]) $row_array[$key] = '0'; // Nulify empty keys 
-			if ($row_array[0] == '0') $row_array[0] = '00:00:00'; // time value cannot be 0 so we set it in time format. Need to rework
-
 		}
 
-		// Remove empty elements
+		// Remove empty keys
 		$row_array = array_filter($row_array, function($value) { return $value !== ''; });
 
-		// We add array data to a new array only if keys count is above 5 to filter data only (skip 'LINUX RESTART' short row, blank rows etc.)
-		if (count($row_array) > 5 ) {
-			// Nasty exception for network
+		// Add array data to output array
+		// By counting keys in array skip short rows related to restart state
+		if (count($row_array) > 3 ) {
+
+			// Skip the 'Average' row that can be seen throw output regularly
+			if ($row_array[0] == 'Average:') continue;
+
+			// Nasty exception for network to select the interface we need (eth0 in the example)
 			if ($atr != '-n DEV') {
 				$out[$key_row] = $row_array;
 			} else {
-				if ($row_array['1'] == "eth0") {
-					$out[$key_row] = $row_array;
-				}
+				if ($row_array['1'] == "eth0") $out[$key_row] = $row_array; // The restart state cannot be seen here. Need to fix somehow
 			}
 		}
 	}
 
-	// Debug
-	//print_r(var_dump($out));
+	// Pretty looking debug (found on stackoverflow)
+	//$pretty = function($v='',$c="&nbsp;&nbsp;&nbsp;&nbsp;",$in=-1,$k=null)use(&$pretty){$r='';if(in_array(gettype($v),array('object','array'))){$r.=($in!=-1?str_repeat($c,$in):'').(is_null($k)?'':"$k: ").'<br>';foreach($v as $sk=>$vl){$r.=$pretty($vl,$c,$in+1,$sk).'<br>';}}else{$r.=($in!=-1?str_repeat($c,$in):'').(is_null($k)?'':"$k: ").(is_null($v)?'&lt;NULL&gt;':"<strong>$v</strong>");}return$r;};		
+	//print_r($pretty($out));
 
 	return $out;
 }
